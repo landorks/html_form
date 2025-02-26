b570394cad205489b13983e12f65224cd082654f
COMLAB <oprincipe23-0385@cca.edu.ph>
2025-02-26 18:01:22 +0800

    htmlformdawnga

---
+++ register.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=<form action="action.php" method="POST">
+    <table>
+        <tr>
+            <td> Name: </td>
+            <td> <input type="text" name="name"> </td>
+        </tr>
+        <tr>
+            <td> Gender: </td>
+            <td>
+                <input type="radio" name="gender" value="male"> Male
+                <input type="radio" name="gender" value="female"> Female
+            </td>
+        </tr>
+        <tr>
+            <td> Country: </td>
+            <td>
+                <select name="country">
+                    <option value="PH">Philippines</option>
+                    <option value="AUS">Australia</option>
+                    <option value="US">United States</option>
+                </select>
+            </td>
+        </tr>
+        <tr>
+            <td colspan="2">
+                <input type="submit" value="Save">
+            </td>
+        </tr>
+    </table>
+</form>
+</head>
+<body>
+    
+</body>
+</html>
